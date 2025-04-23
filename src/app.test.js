ce8570f178481963cc53235655213925458b5e19
Watchirawit Srisoonthornkasem <pluemwitzz@gmail.com>
2025-04-23 15:08:43 +0700

    update app.js and app.test.js

--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,12 @@
 const { dayOfTheWeek } = require('./app');
-
+const { monthOfYear } = require('./app');
 
 test('getDay returns the long-format day of the week', () => {
     const day = dayOfTheWeek( new Date('3/11/2020') );
     expect( day ).toBe('Wednesday');
+});
+
+test('getDay returns the month of the year', () => {
+    const month = dayOfTheWeek( new Date('3/11/2020') );    
+    expect( month ).toBe('November');
 });
